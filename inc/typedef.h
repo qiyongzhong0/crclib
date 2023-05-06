6d037603c4ceac77dc940f42138b2b5ff8b45222
samuelyhsu <samuel.hsu@qq.com>
2023-05-06 14:35:37 +0800

    - replace with <stdint.h>

--- inc/typedef.h
+++ inc/typedef.h
@@ -13,34 +13,8 @@
 extern "C" {
 #endif
 
-//---------------------------------------
-typedef signed char s8;
-typedef signed short s16;
-typedef signed long s32;
-typedef signed long long s64;
+#include <stdint.h>
 
-typedef unsigned char u8;
-typedef unsigned short u16;
-typedef unsigned long u32;
-typedef unsigned long long u64;
-
-typedef float f32;
-typedef double f64;
-
-typedef void (*PHOOK_t)(void);
-
-#ifndef NULL
-#define NULL (void *)0
-#endif
-
-#ifndef true
-#define true 1
-#endif
-
-#ifndef false
-#define false 0
-#endif
-//---------------------------------------
 #ifdef __cplusplus
 }
 #endif
